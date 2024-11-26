c8f6a76ca853a653b3eb0ca7f3825a395aed412a
adragon9 <adragon9@outlook.com>
2024-11-25 23:32:18 -0600

    Fighting dependencies again

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import '@expo/metro-runtime';
 import { NavigationContainer } from "@react-navigation/native";
 // import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
